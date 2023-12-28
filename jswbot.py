51131814cc41e202aa70d554b71c39f445a3cfd4
DESKTOP-LG6V8VG\Welcome <231415@dg1s.hs.kr>
2023-12-28 15:01:58 +0900

    빠른 포기

--- jswbot.py
+++ jswbot.py
@@ -1,4 +1,8 @@
 import discord
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
 
 client = discord.Client()
 
@@ -14,4 +18,21 @@ async def on_message(message):
     if message.content.startswith("!hello"):
         await message.channel.send("Hello!")
 
-client.run("YOUR_DISCORD_TOKEN")
+def run_bot():
+    client.run("YOUR_DISCORD_TOKEN")
+
+@app.route('/')
+def home():
+    return "Discord bot is running!"
+
+def run_flask():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t1 = Thread(target=run_bot)
+    t2 = Thread(target=run_flask)
+    t1.start()
+    t2.start()
+
+if __name__ == "__main__":
+    keep_alive()
